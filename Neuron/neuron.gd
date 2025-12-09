5dfaca12e09d3155e5fe6524882443da865bbca7
TheHat1 <gfs.74.bg@gmail.com>
2025-12-09 18:10:36 +0200

    Add node signal emitting system

--- Neuron/neuron.gd
+++ Neuron/neuron.gd
@@ -1,6 +1,43 @@
 extends GraphNode
 
+var led_off = load("res://Assets/neuron_led.png")
+var led_on = load("res://Assets/neuron_led_lit.png")
+var temp = 100
+
 func _ready():
-	set_slot(0, true, 0, Color.DARK_GOLDENROD, false, 0, Color.FIREBRICK)
+	set_slot(0, true, 0, Color.DARK_GOLDENROD, true, 0, Color.DARK_GOLDENROD)
 	set_slot(1, true, 0, Color.FIREBRICK, true, 0, Color.BLUE_VIOLET)
 	set_slot(2, true, 0, Color.FOREST_GREEN, false, 0, Color.FIREBRICK)
+
+func fire_output(port: int):
+	get_parent().trigger_from(name, port)
+
+func execute_input(port: int):
+	match port:
+		0:
+			on_inpulse_in()
+		1:
+			on_inhib_in()
+		2:
+			on_exc_in()
+		_:
+			print("Unhandled port: ", port)
+
+func _process(_delta):
+	temp += 1
+	if(temp == 101):
+		fire_output(1)
+		temp = 0
+
+func on_inpulse_in():
+	fire_output(0)
+	$HBoxContainer/Control/Sprite2D.texture = led_on
+	$HBoxContainer/Control/AudioStreamPlayer2D.play()
+	await $HBoxContainer/Control/AudioStreamPlayer2D.finished
+	$HBoxContainer/Control/Sprite2D.texture = led_off
+
+func on_inhib_in():
+	pass
+
+func on_exc_in():
+	pass
--- graph_edit.gd
+++ graph_edit.gd
@@ -4,6 +4,7 @@ var neuron = load("res://Neuron/neuron.tscn")
 
 func _ready() -> void:
 	size = get_viewport().get_visible_rect().size
+	
 
 func _input(event: InputEvent):
 	if event is InputEventKey and Input.is_key_pressed(KEY_R):
@@ -16,3 +17,10 @@ func _on_connection_request(from_node: StringName, from_port: int, to_node: Stri
 
 func _on_disconnection_request(from_node: StringName, from_port: int, to_node: StringName, to_port: int) -> void:
 	disconnect_node(from_node, from_port, to_node, to_port)
+
+func trigger_from(from_node: StringName, from_port: int):
+	for connection in get_connection_list():
+		if connection.from_node == from_node and connection.from_port == from_port:
+			var target := get_node(NodePath(connection.to_node))
+			if target.has_method("execute_input"):
+				target.execute_input(connection.to_port)
