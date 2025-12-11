8a54ff7179f7019a3fc271cf0c341fc8e49b9726
TheHat1 <gfs.74.bg@gmail.com>
2025-12-11 09:54:25 +0200

    Add pacemaker

---
+++ PaceMaker/pace_maker.gd
@@ -0,0 +1,27 @@
+extends GraphNode
+
+var isToggled = false
+var interval := 1.0
+var elapsed := 0.0
+var frequency_hz := 1.0
+
+func _ready() -> void:
+	set_slot(0, false, 0, Color.FIREBRICK, true, 0, Color.DARK_GOLDENROD)
+
+func fire_output(port: int):
+	get_parent().trigger_from(name, port)
+
+func _on_check_button_toggled(toggled_on: bool) -> void:
+	isToggled = toggled_on
+
+func _process(delta: float) -> void:
+	if isToggled:
+		elapsed += delta
+		if elapsed >= interval:
+			elapsed -= interval
+			fire_output(0)
+
+func _on_line_edit_text_submitted(new_text: String) -> void:
+	if new_text.to_float() > 0:
+		frequency_hz = new_text.to_float()
+		interval = 1.0 / frequency_hz
--- UI/add_elements_menu.gd
+++ UI/add_elements_menu.gd
@@ -1,6 +1,7 @@
 extends Control
 
 var neuron = load("res://Neuron/neuron.tscn")
+var pacemaker = load("res://PaceMaker/PaceMaker.tscn")
 
 func _on_button_pressed() -> void:
 	var n = neuron.instantiate()
@@ -10,7 +11,10 @@ func _on_button_pressed() -> void:
 
 
 func _on_button_2_pressed() -> void:
-	pass # Replace with function body.
+	var p = pacemaker.instantiate()
+	p.position_offset = position
+	get_parent().add_child(p)
+	get_parent().close_menu_after_inst()
 
 
 func _on_button_3_pressed() -> void:
