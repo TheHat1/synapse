0679790dce610b332f1805da0257af1440d08899
TheHat1 <gfs.74.bg@gmail.com>
2026-01-15 20:55:16 +0200

    Set all lineedit input types accordingly

--- UI/dataset_panel.gd
+++ UI/dataset_panel.gd
@@ -1,9 +1,19 @@
 extends Control
 
 var range_element = load("res://UI/range_element.tscn")
+var ranges:= []
+var hypercube:= []
 
 func _ready() -> void:
 	size = get_viewport().get_visible_rect().size
 
 func _on_button_pressed() -> void:
 	$Panel/MarginContainer/HBoxContainer/OptionsBox/Ranges.add_child(range_element.instantiate())
+
+func _on_generate_button_pressed() -> void:
+	var range_elements = $Panel/MarginContainer/HBoxContainer/OptionsBox/Ranges.get_children()
+	var normaliza = $Panel/MarginContainer/HBoxContainer/OptionsBox/DataNorm/HBoxContainer/DataNormButton.button_pressed
+	var N = $Panel/MarginContainer/HBoxContainer/OptionsBox/SplitN/HBoxContainer/DataNormButton.text.to_int()
+	for r in range_elements:
+		ranges.append({"min": r.minimal,"max": r.maximum})
+	
--- UI/range_element.gd
+++ UI/range_element.gd
@@ -1,5 +1,15 @@
 extends Panel
 
+var minimal = 0.0
+var maximum = 10.0
 
 func _on_button_pressed() -> void:
 	queue_free()
+
+func _on_min_text_submitted(new_text: String) -> void:
+	if new_text.to_float() >= 0:
+		minimal = new_text.to_float()
+
+func _on_max_text_submitted(new_text: String) -> void:
+	if new_text.to_float() >= 0 and new_text.to_float() > minimal:
+		maximum = new_text.to_float()
