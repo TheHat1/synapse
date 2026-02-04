14e5f8508e84715a826e1c4f81e4264d9fb52f26
TheHat1 <gfs.74.bg@gmail.com>
2026-02-04 11:41:27 +0200

    Add output ranges generator

--- UI/dataset_panel.gd
+++ UI/dataset_panel.gd
@@ -1,20 +1,21 @@
 extends Control
 
 var range_element = load("res://UI/range_element.tscn")
-var hypercube:= []
+var hypercubeIR:= []
+var hypercubeOR:= []
 
 func _ready() -> void:
 	size = get_viewport().get_visible_rect().size
 
 func _on_button_pressed() -> void:
-	$Panel/MarginContainer/HBoxContainer/OptionsBox/ScrollContainer/Ranges.add_child(range_element.instantiate())
+	$Panel/ScrollContainer/MarginContainer/VBoxContainer/HBoxContainer/OptionsBox/ScrollContainer/Ranges.add_child(range_element.instantiate())
 
 func _on_generate_button_pressed() -> void:
-	hypercube = []
-	var range_elements = $Panel/MarginContainer/HBoxContainer/OptionsBox/ScrollContainer/Ranges.get_children()
-	var normalize = $Panel/MarginContainer/HBoxContainer/OptionsBox/DataNorm/HBoxContainer/DataNormButton.button_pressed
-	var dds = $Panel/MarginContainer/HBoxContainer/OptionsBox/DDS/HBoxContainer/DataNormButton.button_pressed
-	var N = $Panel/MarginContainer/HBoxContainer/OptionsBox/SplitN/HBoxContainer/DataNormButton.text.to_int()
+	hypercubeIR = []
+	var range_elements = $Panel/ScrollContainer/MarginContainer/VBoxContainer/HBoxContainer/OptionsBox/ScrollContainer/Ranges.get_children()
+	var normalize = $Panel/ScrollContainer/MarginContainer/VBoxContainer/HBoxContainer/OptionsBox/DataNorm/HBoxContainer/DataNormButton.button_pressed
+	var dds =$Panel/ScrollContainer/MarginContainer/VBoxContainer/HBoxContainer/OptionsBox/DDS/HBoxContainer/DataNormButton .button_pressed
+	var N = $Panel/ScrollContainer/MarginContainer/VBoxContainer/HBoxContainer/OptionsBox/SplitN/HBoxContainer/SplitStep.text.to_int()
 	var rng = RandomNumberGenerator.new()
 	var colors = []
 	
@@ -37,12 +38,30 @@ func _on_generate_button_pressed() -> void:
 					range_steps.append(r.minimal + (i-0.5) * delta)
 				else:
 					range_steps.append(r.minimal + (i-rng.randf()) * delta)
-		hypercube.append(range_steps)
+		hypercubeIR.append(range_steps)
 		range_steps = []
-	for i in hypercube.size():
+	for i in hypercubeIR.size():
 		if i != 0: 
 			for j in range(0, i):
-				var value_start = hypercube[i].pop_front()
-				hypercube[i].append(value_start)
-	$Panel/MarginContainer/HBoxContainer/GraphDisplay/MarginContainer/Control.pass_hypercube(hypercube, colors, N)
-	$Panel/MarginContainer/HBoxContainer/GraphDisplay/Label.text = "Input ranges' 2D hypercube representation: split_steps: "+ str(N) +", data_norm: "+ str(normalize) +", dds: " + str(dds)
+				var value_start = hypercubeIR[i].pop_front()
+				hypercubeIR[i].append(value_start)
+	$Panel/ScrollContainer/MarginContainer/VBoxContainer/HBoxContainer/GraphDisplay/MarginContainer/Control.pass_hypercube(hypercubeIR, colors, N)
+	$Panel/ScrollContainer/MarginContainer/VBoxContainer/HBoxContainer/GraphDisplay/Label.text = "Input ranges' 2D hypercube representation: split_steps: "+ str(N) +", data_norm: "+ str(normalize) +", dds: " + str(dds)
+
+func _on_generate_button_or_pressed() -> void:
+	hypercubeOR = []
+	var range_elements = $Panel/ScrollContainer/MarginContainer/VBoxContainer/HBoxContainer2/OptionsBox/ScrollContainer/Ranges.get_children()
+	var N = $Panel/ScrollContainer/MarginContainer/VBoxContainer/HBoxContainer/OptionsBox/SplitN/HBoxContainer/SplitStep.text.to_int()
+	var colors = []
+	
+	for r in range_elements:
+		colors.append(r.color)
+	
+	for r in range_elements:
+		
+		var range_steps = []
+		
+		for i in range(1, N+1):
+			pass
+		hypercubeOR.append(range_steps)
+		range_steps = []
